27ee115f78d42b54f3d7d5440857bdf83f17862b
miracleexotic <mai_nutthawat@hotmail.com>
2021-08-20 13:59:04 +0700

    releases v2.0.0

--- v2/quote.go
+++ v2/quote.go
@@ -3,10 +3,10 @@ package quote
 // v1.0.0
 // Say : say Hello
 func Say2() string {
-	return "Hello22"
+	return "Hello2"
 }
 
 // Speak : say Hi, mai
 func Speak2() string {
-	return "Hi, mai22"
+	return "Hi, mai2"
 }
